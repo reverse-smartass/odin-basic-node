6e5934a5ba73684b145404b8917e8ece88ab63b6
reverse-smartass <nguyenminhdore@gmail.com>
2026-03-06 15:00:45 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 const app = express();
 
 const port = process.env.PORT || 3000;
+const pages = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Contact", path: "/contact" }
+];
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'src/views'));
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "src/views"));
+app.use(express.static(path.join(process.cwd(),"src","public")));
 
-app.get('/', (req, res) => {
-  res.render('homepage');
+app.get("/", (req, res) => {
+  res.render("homepage", { pages });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about", { pages });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
+app.get("/contact", (req, res) => {
+  res.render("contact", { pages });
 });
 
 app.use((req, res) => {
-  res.status(404).render('404', { title: 'Page Not Found' });
+  res.status(404).render("404", { title: "Page Not Found", pages });
 });
 
-app.listen(port, () => {
+app.listen(port, (error) => {
+  if (error) {
+    throw error;
+  }
   console.log(`Server is running on port ${port}`);
 });
